14eba0c9ed409b0ff6b4000a10da6f94c5450e10
patrick jesssen <patrick-j1@hotmail.dk>
2020-12-14 14:38:20 +0100

    Add project files.

---
+++ Server/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listen = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.port = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // listen
+            // 
+            this.listen.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listen.FlatAppearance.BorderSize = 3;
+            this.listen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.listen.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listen.ForeColor = System.Drawing.Color.Gainsboro;
+            this.listen.Location = new System.Drawing.Point(0, 347);
+            this.listen.Name = "listen";
+            this.listen.Size = new System.Drawing.Size(697, 27);
+            this.listen.TabIndex = 16;
+            this.listen.Text = "Listen";
+            this.listen.UseVisualStyleBackColor = true;
+            this.listen.Click += new System.EventHandler(this.listen_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label3.Location = new System.Drawing.Point(285, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 16);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Port Number";
+            // 
+            // port
+            // 
+            this.port.Location = new System.Drawing.Point(279, 202);
+            this.port.Name = "port";
+            this.port.Size = new System.Drawing.Size(100, 20);
+            this.port.TabIndex = 17;
+            this.port.TextChanged += new System.EventHandler(this.port_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 374);
+            this.Controls.Add(this.port);
+            this.Controls.Add(this.listen);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button listen;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox port;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ Server/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listen_Click(object sender, EventArgs e)
+        {
+            new Form2(int.Parse(port.Text)).Show();
+            listen.Enabled = false;
+        }
+
+        private void port_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
